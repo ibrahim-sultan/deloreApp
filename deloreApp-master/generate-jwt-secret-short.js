0f5e6f335edba00a34825f86be9502eb270185af
Ibrahim Sultan <79456590+ibrahim-sultan@users.noreply.github.com>
2025-10-17 20:01:38 +0100

    Delete deloreApp-master directory

--- deloreApp-master/generate-jwt-secret-short.js
+++
@@ -1,6 +0,0 @@
-const crypto = require('crypto');
-
-// Generate a secure 128-bit (16 bytes) random string for JWT_SECRET
-const jwtSecret = crypto.randomBytes(16).toString('hex');
-
-console.log('JWT_SECRET=' + jwtSecret);
--- deloreApp-master/generate-jwt-secret.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto');
-
-// Generate a secure 256-bit (32 bytes) random string for JWT_SECRET
-const jwtSecret = crypto.randomBytes(32).toString('hex');
-
-console.log('JWT_SECRET=' + jwtSecret);
-console.log('Copy this value to your .env file');
